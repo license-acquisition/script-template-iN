9935944a9d8d975fb882135666ecf0d589d79266
TylerEto <tyler.eto@gmail.com>
2015-03-13 09:15:36 -0700

    added close to logger function

--- script_template/feed_log/feed_log.py
+++ script_template/feed_log/feed_log.py
@@ -3,3 +3,4 @@ import time, codecs
 def log(feed_name, status, error_string=''):
     l = codecs.open('log.csv', 'a')
     l.write(','.join([feed_name, str(time.strftime('%Y/%m/%d')), str(time.strftime('%H:%M:%S')), status, error_string]) + '\n')
+    l.close()
