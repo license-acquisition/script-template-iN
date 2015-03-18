e47e2b5d3cb420dca70635ded22723286d0256d9
TylerEto <tyler.eto@gmail.com>
2015-03-17 17:33:55 -0700

    changes to modules
    - writes output to output folder
    - only one logger now (not l and h)

--- script_template/feed_log/feed_log.py
+++ script_template/feed_log/feed_log.py
@@ -1,39 +1,27 @@
 import time, codecs, logging
 
-'''
-def logger(feed_name, status, error_string=''):
-    l = codecs.open('log.csv', 'a')
-    l.write(','.join([feed_name, str(time.strftime('%Y/%m/%d')), str(time.strftime('%H:%M:%S')), status, error_string]) + '\n')
-    l.close()
-'''
-
-def logger(feed):
+def logger(feed, master='N'):
     # set up loggers
     logger = logging.getLogger(str(feed))
     logger.setLevel(logging.DEBUG)
-    helper = logging.getLogger('HELPER')
-    helper.setLevel(logging.DEBUG)
 
     # create handlers
-    ch1 = logging.StreamHandler()
-    ch1.setLevel(logging.CRITICAL)
-    ch2 = logging.StreamHandler()
-    ch2.setLevel(logging.DEBUG)
-    fh1 = logging.FileHandler('main.log')
-    fh1.setLevel(logging.DEBUG)
-    fh2 = logging.FileHandler('%s.log' %feed)
-    fh2.setLevel(logging.DEBUG)
-
+    if master == 'N': # general use: individual script
+        fh = logging.FileHandler('script_logs/%s.log' %feed)
+        fh.setLevel(logging.DEBUG)
+    else:
+        fh = logging.FileHandler('main_%s.log' %time.strftime('%Y-%m-%d'))
+        fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler() # logs to console all Critical errors
+    ch.setLevel(logging.CRITICAL)    
+    
     # format and add to handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                                  '%m/%d/%Y %H:%M:%S')
-    for handler in [ch, fh1, fh2]:
+                                  '%Y/%m/%d %H:%M:%S')
+    for handler in [fh, ch]:
         handler.setFormatter(formatter)
 
-    # add handlers to loggers
-    logger.addHandler(ch1)
-    logger.addHandler(fh1)
-    helper.addHandler(ch2)
-    helper.addHandler(fh2)
-
-    return logger, helper
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    
+    return logger
--- script_template/template.py
+++ script_template/template.py
@@ -3,9 +3,9 @@ from headers import get_headers
 
 def create_file(feed_name, permission, headers_array):
     if permission == 'a':
-        f = codecs.open(feed_name, permission, 'utf-8')
+        f = codecs.open('output/%s' %feed_name, permission, 'utf-8')
     else:
-        f = codecs.open(feed_name+'_%s_000.csv'%(time.strftime('%Y%m%d')), permission, 'utf-8')
+        f = codecs.open('output/%s_%s_000.csv'%(feed_name, time.strftime('%Y%m%d')), permission, 'utf-8')
     headers = get_headers(headers_array)
     f.write('|'.join(headers) + '\n')
     return f
