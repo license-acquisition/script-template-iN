d46393927fb2820e5b068dceb7403ff885074170
onejun <onejun.ong@gmail.com>
2015-04-02 18:46:22 -0700

    Implemented Daryl's changes to converted file.
    arc-ind-lar_b_IAiwd

--- source scripts/sel-range/arc-ind-lar_b_IAiwd.py
+++ source scripts/sel-range/arc-ind-lar_b_IAiwd.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from script_template import create_file, logger
-f = create_file("arc-ind-lar_b_IAiwd","w",[12,0,1,4,36,44,33,66,21,"entity_type",37,13,19,"licensed_by"])
+f = create_file("arc-ind-lar_b_IAiwd","w",[12,0,1,4,36,44,33,66,21,7,37,13,19,"licensed_by"])
 l = logger('IAiwd')
 browser = webdriver.PhantomJS()
 
-
 def main():
+	# remove_list is to substitute empty spaces that should have information with no_info
+	remove_list = [1,11,12] 
+
+	#4,8,9 refers to architects, interior desingers, and landscape architects
 	for i in (4,8,9):
 		start = 1
 		end = 30000
@@ -27,33 +30,41 @@ def main():
 			
 					for contact_info in soup.find_all("table")[2].find_all("tr")[1:]:
 						for td in contact_info.find_all("td"):
-							if td == "&nbsp":
-								pass
-							elif td.has_attr("class"):
-								pass
+							if td.has_attr("class"):
+								continue
 							else:
 								info.append(td.text.strip())
-								break
+								
 
 					for license_info in soup.find_all("table")[4].find_all("tr"):
 						for td in license_info.find_all("td"):
 							if td.has_attr("class"):
-								pass
+								continue
 							else:
 								info.append(td.text.strip()) 
-								break
+								
 
 					for issue_info in soup.find_all("table")[5].find_all("tr"):
 						for td in issue_info.find_all("td"):
 							if td.has_attr("class"):
-								pass
-					
+								continue
 							else:
-								
 								info.append(td.text.strip())
-								break
-						
-					info = [x for x in info if len(x) !=0]	
+					for remove in remove_list:
+						if info[remove]=="":
+							info[remove] = "no_info"	#substitute whitespaces that should have information with no_info to distinguish from below
+					info = [x for x in info if len(x) !=0]	#to remove whitespaces that are decoration-only on the site
+					for k in range(1,len(info)):
+						if "Firm" in info[k]:
+							info[i]="1"
+						elif "Individual" in info[k]:
+							info[i]="0"
+
+					for index in range(0,len(info)):
+						if info[index] == "no_info":
+							info[index] = ""
+
+
 					f.write("|".join(info) + "\n")
 					print info
 					l.info('|'.join(info) + "\n")
